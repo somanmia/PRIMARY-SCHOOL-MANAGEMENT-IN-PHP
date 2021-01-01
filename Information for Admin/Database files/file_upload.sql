d2ed3f4d666e7d9b197a4ef5c9beb31cd2c64912
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2020-12-31 20:19:55 -0800

    Update file_upload.sql

--- Information for Admin/Database files/file_upload.sql
+++ Information for Admin/Database files/file_upload.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2020 at 04:06 AM
+-- Generation Time: Jan 01, 2021 at 05:02 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -36,34 +36,6 @@ CREATE TABLE `file_upload` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `file_upload`
---
-
-INSERT INTO `file_upload` (`Id`, `Title`, `Date`, `File_name`) VALUES
-(1, 'SSC Result', '0012-12-12', 'Problem 17 - [৫২ সমস্যা বই] স্বরবর্ণ গণনা - Dimik OJ.pdf'),
-(2, 'SSC Result', '2020-05-05', '1.pdf'),
-(3, 'SSC Result', '2020-05-05', '1.pdf'),
-(4, 'SSC Result', '2020-05-05', '1.pdf'),
-(5, 'SSC Result', '2020-05-05', '1.pdf'),
-(6, 'SSC Result', '2020-05-05', '1.pdf'),
-(7, 'SSC Result', '2020-05-05', '1.pdf'),
-(8, 'SSC Result', '2020-05-05', '1.pdf'),
-(9, 'SSC Result', '2020-05-05', '1.pdf'),
-(10, 'SSC Result', '2020-05-05', '1.pdf'),
-(11, 'SSC Result', '2020-05-05', '1.pdf'),
-(12, 'SSC Result', '2020-05-05', '1.pdf'),
-(13, 'SSC Result', '2020-05-05', '1.pdf'),
-(14, 'SSC Result', '2020-05-05', '1.pdf'),
-(15, 'SSC Result', '2020-05-05', '1.pdf'),
-(16, 'SSC Result', '2020-05-05', '1.pdf'),
-(17, 'SSC Result', '2020-05-05', '1.pdf'),
-(18, 'jsc Result', '2020-05-18', '1.pdf'),
-(19, 'jsc Result', '2020-05-18', '1.pdf'),
-(20, 'Examination-2', '2020-12-02', 'hh.pdf'),
-(21, 'Examination-3', '2020-12-03', 'hh.pdf'),
-(22, 'Examination-4', '2020-12-04', 'hh.pdf');
-
---
 -- Indexes for dumped tables
 --
 
@@ -81,7 +53,7 @@ ALTER TABLE `file_upload`
 -- AUTO_INCREMENT for table `file_upload`
 --
 ALTER TABLE `file_upload`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
