b880a61d2cdd8651a4c0dc301889ccc366bb5853
Md:Soman Mia <59284020+somanmia@users.noreply.github.com>
2021-01-01 18:25:18 -0800

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+//Counter up section--------------//
+$(document).ready(function(){
+  $(".count").counterUp({
+      delay:40,
+      time:6000
+
+
+
+  });
+
+
+});
+//end counterup section...............//
